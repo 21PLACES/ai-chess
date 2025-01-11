1652ca56dfdf1d2089562620023c107649c81479
Азим <97340544+AzimAkhmedov@users.noreply.github.com>
2025-01-11 13:59:11 +0500

    FEAT: pieces kernels & default methods

--- game/board.py
+++ game/board.py
@@ -1,5 +1,5 @@
 
-from game.pieces import Rock, Knight, Bishop, Queen, King, Pawn
+from pieces import Rook, Knight, Bishop, Queen, King, Pawn, get_attacked_pieces, get_piece_by_position
 
 available_pieces = ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Pawn']
 available_pos_states = ['Empty', 'null']
@@ -10,24 +10,55 @@ class BoardFaker:
         self.whitesTook = []
         self.blacksTook = []
         self.gameState = 'PlayingWhite'
+        self.turn  = "white"
+        self.castlingWhite = True
+        self.castlingBlack = True
+         
         self.board = [
-            [Rock([0, 0], 'black'), Knight([0, 1], 'black'), Bishop([0, 2], 'black'), Queen([0, 3], 'black'), King([0, 4], 'black'), Bishop([0, 5], 'black'), Knight([0, 6], 'black'), Rock([0, 7], 'black')],
+            [Rook([0, 0], 'black'), Knight([0, 1], 'black'), Bishop([0, 2], 'black'), Queen([0, 3], 'black'), King([0, 4], 'black'), Bishop([0, 5], 'black'), Knight([0, 6], 'black'), Rook([0, 7], 'black')],
             [Pawn([1, 0], 'black'), Pawn([1, 1], 'black'), Pawn([1, 2], 'black'), Pawn([1, 3], 'black'), Pawn([1, 4], 'black'), Pawn([1, 5], 'black'), Pawn([1, 6], 'black'), Pawn([1, 7], 'black')],
             ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
             ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
             ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
             ['Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty', 'Empty'],
             [Pawn([6, 0], 'white'), Pawn([6, 1], 'white'), Pawn([6, 2], 'white'), Pawn([6, 3], 'white'), Pawn([6, 4], 'white'), Pawn([6, 5], 'white'), Pawn([6, 6], 'white'), Pawn([6, 7], 'white')],
-            [Rock([7, 0], 'white'), Knight([7, 1], 'white'), Bishop([7, 2], 'white'), Queen([7, 3], 'white'), King([7, 4], 'white'), Bishop([7, 5], 'white'), Knight([7, 6], 'white'), Rock([7, 7], 'white')]
-        ]
-        
+            [Rook([7, 0], 'white'), Knight([7, 1], 'white'), Bishop([7, 2], 'white'), Queen([7, 3], 'white'), King([7, 4], 'white'), Bishop([7, 5], 'white'), Knight([7, 6], 'white'), Rook([7, 7], 'white')]
+        ]   
         
     def print_board(self):
         for row in self.board:
-            print(row)
+            for piece in row:
+                if piece != 'Empty':
+                    print(piece.get_name(), end=' ')
+                else:
+                    print(piece, end=' ')
+            print('\n')
+    
+    
+    def get_piece_by_position(self, position):
+        return get_piece_by_position(self.board, position)        
+    
+    def move_piece(self, origin, destination):
+        piece = self.get_piece_by_position(origin)
+        if piece == 'Empty':
+            return
+        if piece.get_color() != self.turn:
+            return
+        
+        if not piece.is_valid_move(destination, self.board):
+            return
+        
+        self.board[destination[0]][destination[1]] = piece
+        self.board[origin[0]][origin[1]] = 'Empty'
+        piece.position = destination
+        self.gameState = 'PlayingBlack' if self.gameState == 'PlayingWhite' else 'PlayingWhite'
+            
 
 
 board_faker = BoardFaker()
 
-
 board_faker.print_board()
+
+rook = board_faker.get_piece_by_position([0, 0])
+
+print(get_attacked_pieces(board_faker.board, [0, 0], rook.get_kernel(),  rook.get_kernel_type(), rook.get_color()))
--- game/pieces.py
+++ game/pieces.py
@@ -1,5 +1,65 @@
+def kernel_convolution(kernel, board, x, y):
+    result = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if is_valid_boundary(x + i, y + j):
+                result += kernel[i + 1][j + 1] * board[x + i][y + j]
+    return result
 
+def get_piece_by_position(board, position):
+    x, y = position
+    return board[x][y]
 
+def validate_move_by_kernel(kernel,kernelType, board, x, y):
+    if kernelType == 'Continuous':
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if kernel[i][j] == 1 and board[x + i - 1][y + j - 1] != 'Empty':
+                    return False
+    else:
+        for i in range(0, 5):
+            for j in range(0, 5):
+                if kernel[i][j] == 1 and board[x + i - 2][y + j - 2] != 'Empty':
+                    return False
+    return True
+    
+
+def get_attacked_pieces(board, position, kernel, kernel_type, color):
+    """
+    Determines the pieces under attack based on the kernel of a chess piece.
+
+    Args:
+        board (list[list]): 2D list representing the chessboard.
+        position (tuple): (row, col) of the piece's position.
+        kernel (list or list[tuple]): The kernel defining the attack pattern.
+        kernel_type (str): "Continuous" or "Discrete".
+
+    Returns:
+        list[tuple]: Positions of the pieces under attack.
+    """
+    rows, cols = len(board), len(board[0])
+    attacked = []
+
+    row, col = position
+
+    if kernel_type == "Discrete":
+        for dr, dc in kernel:
+            r, c = row + dr, col + dc
+            if 0 <= r < rows and 0 <= c < cols and board[r][c] is not None and board[r][c].color != color:
+                attacked.append((r, c))
+
+    elif kernel_type == "Continuous":
+        for dr, dc in kernel:
+            r, c = row + dr, col + dc
+            while 0 <= r < rows and 0 <= c < cols:
+                if board[r][c] != "Empty":
+                    if board[r][c].color != color:
+                        attacked.append((r, c))
+                    break
+                r += dr
+                c += dc
+
+    return attacked
 
 
 
@@ -9,15 +69,13 @@ def is_valid_boundary(x, y):
 def is_same_position(position1, position2):
     return position1[0] == position2[0] and position1[1] == position2[1]
 
-class Rock:
+class Rook:
     def __init__(self, position, color):
         self.position = position
         self.color = color
-        self.kernel = [[0, 1, 0],
-                       [1, 0 ,1],
-                       [0, 1, 0]]
+        self.kernel = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         self.kernelType = 'Continuous' 
-    
+        
     def is_valid_move(self, new_position, board):
         x1, y1 = self.position
         x2, y2 = new_position
@@ -44,27 +102,28 @@ class Rock:
             return True
         return False
     
+    def move(self, new_position, board):
+        if not self.is_valid_move(new_position, board):
+            return False
+        x1, y1 = self.position
+        x2, y2 = new_position
+        board[x1][y1] = 'Empty'
+        self.position = new_position
+        board[x2][y2] = self
+        return True    
+    def get_kernel(self):
+        return self.kernel
+    def get_kernel_type(self):
+        return self.kernelType
+    def get_position(self):
+        return self.position
+    def get_color(self):
+        return self.color
     def get_atacking_pieces(self, board):
-        x, y = self.position
-        pieces = []
-        for i in range(0, x):
-            if board[i][y] != 'Empty':
-                pieces.append(board[i][y])
-                break
-        for i in range(x + 1, 8):
-            if board[i][y] != 'Empty':
-                pieces.append(board[i][y])
-                break
-        for i in range(0, y):
-            if board[x][i] != 'Empty':
-                pieces.append(board[x][i])
-                break
-        for i in range(y + 1, 8):
-            if board[x][i] != 'Empty':
-                pieces.append(board[x][i])
-                break
-        return pieces            
-            
+        return get_attacked_pieces(board, self.position, self.kernel, self.kernelType)           
+    def get_name(self):
+        return self.color+ ' Rook'
+    
 
         
 
@@ -73,52 +132,76 @@ class Knight:
     def __init__(self, position, color):
         self.position = position
         self.color = color
-        self.kernel = [[0, 1, 0, 1, 0],
-                       [1, 0, 0, 0, 1],
-                       [0, 0, 0, 0, 0],
-                       [1, 0, 0, 0, 1],
-                       [0, 1, 0, 1, 0]]
+        self.kernel = [
+            (-2, -1), (-1, -2), (1, -2), (2, -1),
+            (2, 1), (1, 2), (-1, 2), (-2, 1)
+        ]
         self.kernelType = 'Discrete'
-        
+    def get_kernel(self):
+        return self.kernel
+    def get_kernel_type(self):
+        return self.kernelType
+    def get_position(self):
+        return self.position
+    def get_color(self):
+        return self.color
+    def get_atacking_pieces(self, board):
+        return get_attacked_pieces(board, self.position, self.kernel, self.kernelType)    
+    def get_name(self):
+        return self.color + ' Knight'
+
 class Bishop:
     def __init__(self, position, color):
         self.color = color
         self.position = position
-        self.kernel = [[1, 0, 1],
-                       [0, 0, 0],
-                       [1, 0, 1]]
+        self.kernel = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
         self.kernelType = 'Continuous'
-
+    def get_kernel(self):
+        return self.kernel
+    def get_kernel_type(self):
+        return self.kernelType
+    def get_position(self):
+        return self.position
+    def get_color(self):
+        return self.color
+    def get_atacking_pieces(self, board):
+        return get_attacked_pieces(board, self.position, self.kernel, self.kernelType)    
+    def get_name(self):
+        return self.color + ' Bishop'
 
 class Queen:
     def __init__(self, position, color):
         self.position = position
         self.color = color
-        self.kernel = [[1, 1, 1],
-                       [1, 0, 1],
-                       [1, 1, 1]]
+        self.kernel = [
+                    (-1, 0), (1, 0), (0, -1), (0, 1),  
+                    (-1, -1), (-1, 1), (1, -1), (1, 1)
+                ]  
         self.kernelType = 'Continuous'
+    def get_name(self):
+        return self.color + ' Queen'
 
 class King:
     def __init__(self, position, color):
         self.color = color
         self.position = position
-        self.kernel = [[1, 1, 1],
-                       [1, 0, 1],
-                       [1, 1, 1]]
+        self.kernel = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1)]
         self.kernelType = 'Discrete'
         self.isUnderAttack = False
+    def get_name(self):
+        return self.color + ' King'
 
 class Pawn:
     def __init__(self, position, color):
         self.position = position
         self.kernelType = 'Discrete'
         self.isUnderAttack = False
+        self.color = color
         if color == 'white':
-            self.kernel = [[1, 1, 1],
-                           [0, 0, 0],
-                           [0, 0, 0]]
+            self.kernel = [(-1, -1), (-1, 1)]
         else :
-            self.kernel = [[0, 0, 0],
-                           [0, 0, 0],
-                           [1, 1, 1]]    
+            self.kernel = [(1, -1), (1, 1)]
+    
+    def get_name(self):
+        return self.color + ' Pawn'    
